a608464a36559175a253563eb42ca31b9b54d45d
stamby23 <filip.stambolov@gmail.com>
2023-10-25 17:32:06 +0300

    added basic body components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import Navbar from "./Navigation/Navbar.js";
+import Home from "./Body/Home.js";
+import About from "./Body/About.js";
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      <Home />
+      <About />
     </div>
   );
 }
 
 export default App;
-
